57192ce7d8a6076d2593b26aeb5f2c438a5ef148
Iynguuuuu <140171430+Iynguuuuu@users.noreply.github.com>
2024-02-22 13:18:56 +0530

    Creted Mobile navbar and onclick events

---
+++ Components/Navbar.tsx
@@ -0,0 +1,27 @@
+import { Bars3Icon } from '@heroicons/react/16/solid'
+import React from 'react'
+
+interface Props {
+    openNav: () => void;
+}
+
+const Navbar = ({ openNav }: Props) => {
+    return (
+        <div className="w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md">
+            <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
+                <h1 className='flex-[0.6] cursor-pointer text[25px] text-white font-bold'>
+                    <span className='text-[#d1b60c]'>SOFTWARE ENGINEERING</span> Undergraduate
+                </h1>
+                <div className='nav-link'>Home</div>
+                <div className='nav-link'>ABOUT</div>
+                <div className='nav-link'>PROJECTS</div>
+                <div className='nav-link'>CONTACT</div>
+                <div onClick={openNav}>
+                    <Bars3Icon className='w-[2rem] md:hidden h-[2rem] cursor-pointer text-yellow-300' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '@/Components/Navbar'
+import MobileNav from '@/Components/MobileNav';
+import Hero from '@/Components/Hero';
 
 const HomePage = () => {
+
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
+
   return (
-    <div className='text-9xl text-red-900'>
-      HomePage
+    <div className="overflow-x-hidden">
+      <div>
+        {/* Navabr */}
+        <MobileNav nav={nav} closeNav={closeNav} />
+        <Navbar openNav={openNav} />
+        {/* Hero Section */}
+        <Hero />
+      </div>
     </div>
   )
 }
