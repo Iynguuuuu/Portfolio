fe530cf440140a68293e2700713d30a88b720c19
Iynguuuuu <140171430+Iynguuuuu@users.noreply.github.com>
2024-02-24 12:28:28 +0530

    Added github and linkedIn links

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -3,18 +3,20 @@ import Particle from './Particle';
 import TextEffect from './TextEffect';
 import Image from 'next/image';
 import { ArrowDownTrayIcon } from '@heroicons/react/16/solid';
+import { IoLogoGithub } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa";
 // import { PiHandWavingLight } from "react-icons/pi";
 
 
 
 const Hero = () => {
     return (
-        <div className='h-[88vh]  bg-[#0f141c]  bg-cover bg-center'> {/*bg-[url(/images/bg.jpg)]*/}
+        <div className='h-[87.5vh]  bg-[url(/images/bg.jpg)]  bg-cover bg-center'> {/**/}
             <Particle />
             <div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
                 <div>
                     <h1 className='text-[35px] md:text-[50px] text-white font-bold'>
-                        HI, I'M <span className='text-yellow-400'>IYNKARAN!</span>
+                        HI, I AM <span className='text-yellow-400'>IYNKARAN!</span>
                     </h1>
                     <TextEffect />
                     <p className='mt-[1.5rem] text-[18px] text-[#ffffff92]'>
@@ -22,13 +24,17 @@ const Hero = () => {
                         Proficient in various programming languages and technologies, with a strong academic foundation in software development principles.
                         Experienced in collaborative projects and eager to contribute to innovative projects in the field.
                     </p>
+                    <div className='mt-[1rem] flex items-stretch space-x-5  '>
+                        <a href="https://github.com/Iynguuuuu"> <IoLogoGithub className='w-[3rem] h-[3rem] text-white' /></a>
+                        <a href="https://lk.linkedin.com/in/iynkaran-pavanantham-b54991263"> < FaLinkedin className='w-[3rem] h-[3rem] text-white' /></a>
+                    </div>
                     <div className='mt-[2rem] flex-col space-x-6 sm:space-y-0  sm:flex sm:flex-row items-center sm:space-x-6'>
                         <button className='px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] 
                         text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2 '>
                             <p>Download CV</p>
                             <ArrowDownTrayIcon className='w-[1.6rem] h-[1.7rem] text-black' />
                         </button>
-                        
+
                     </div>
 
                 </div>
