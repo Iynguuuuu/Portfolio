a805ebda4aee5f97e69d58854c16799186cd7285
Iynguuuuu <140171430+Iynguuuuu@users.noreply.github.com>
2024-02-27 11:13:28 +0530

    Swaped Navbar

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Bars3Icon } from '@heroicons/react/16/solid'
 import { RiCodeSSlashFill } from "react-icons/ri";
-import React from 'react'
+import React from 'react';
 
 interface Props {
     openNav: () => void;
@@ -9,18 +9,20 @@ interface Props {
 const Navbar = ({ openNav }: Props) => {
     return (
         <div className="w-[100%] fixed z-[10000] top-0 h-[12vh] bg-[#090f18] shadow-md">
-            <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
+            <div className='flex items-center justify-between w-[90%] mx-auto h-[100%]'>
                 <h1 className='flex-[0.6]  text-violet-300 text-[60px] font-bold'>
                     <RiCodeSSlashFill />
                 </h1>
                 <div className='nav-link'>Home</div>
                 <div className='nav-link'>SKILLS</div>
-                <div className='nav-link'>SERVICES</div>
                 <div className='nav-link'>PROJECTS</div>
+                <div className='nav-link'>SERVICES</div>
                 <div className='nav-link'>CONTACT</div>
+
                 <div onClick={openNav}>
                     <Bars3Icon className='w-[2rem] md:hidden h-[2rem] cursor-pointer text-yellow-300' />
                 </div>
+
             </div>
         </div>
     )
